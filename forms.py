551afdb87a1f55420d6be9c98738e4e4e2bfaeb2
Leommm-byte <leom20031@gmail.com>
2023-12-18 22:44:55 +0100

    Changed Database to Postgres

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
+from wtforms.fields.html5 import EmailField
 
 
 # WTForm
